01ce0147d15705a79b110c1cd13594d5be5b584e
Can Gündüz <cangunduz0001@gmail.com>
2025-02-07 22:40:19 +0300

    Navbar | Dark Mode

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "../general/ThemeToggle";
+export function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src="/logo.png" alt="Gündüz Job Portal" width={70} height={70} />
+        <h1 className="text-2xl font-bold">
+          Gündüz<span className="text-primary">Job Portal</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Link href="/about">About</Link>
+        <Link href="/jobs">Jobs</Link>
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
+}
